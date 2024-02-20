aa773307b04adfbbbe37d892d51e2ae2ba6b9a99
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-02-20 21:56:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo "Cleaning Up HostScript Files..."
 rm -i ./hostscript/terminal.bat
 echo "Moving HostScript Installation..."
 move /Y %cd%\hostscript %ProgramFiles%
+mv ./hostscript ~/.hostscript
 echo "HostScript Installed."
 read -n 1 -s -r -p "Press any key to quit the installer."
 exit
