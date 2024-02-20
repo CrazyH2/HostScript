e52d61a922730570eadd570592509f57eb844556
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-02-20 21:59:28 +0000

    Update terminal.sh

--- hostscript/terminal.sh
+++ hostscript/terminal.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-printf '\e]1;%s\a' 'HostScript Terminal'
-printf "\e[0;33m"
-clear
-node ./terminal.js "$@"
+function hs() {
+  printf '\e]1;%s\a' 'HostScript Terminal'
+  printf "\e[0;33m"
+  clear
+  node ./terminal.js "$@"
+}
