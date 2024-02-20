e21070adaf9cf63e046a1cc978467a8b5ba53621
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-02-20 21:17:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/bash
 
+printf '\e]1;%s\a' 'HostScript Installer'
+printf "\e[0;33m"
+clear
+echo "Installing HostScript..."
+echo "Cleaning Up HostScript Files..."
+rm -i ./hostscript/terminal.bat
+echo "Moving HostScript Installation..."
+move /Y %cd%\hostscript %ProgramFiles%
+echo "HostScript Installed."
+read -n 1 -s -r -p "Press any key to quit the installer."
+exit
