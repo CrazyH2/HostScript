fdae5bed082c2f67fa85976fe4b6d9a65f803d67
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-02-20 21:13:52 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,10 @@
-
+title "HostScript Installer"
+color e
+cls
+echo "Installing HostScript..."
+echo "Cleaning Up HostScript Files..."
+del /f %cd%\hostscript\terminal.sh
+echo "Moving HostScript Installation..."
+move /Y %cd%\hostscript %ProgramFiles%
+echo "HostScript Installed."
+pause
